4e1f0056cc9ac7aac6aa7493a0e9a7c64371fe60
ImpulseImp <ImpulsEmbedded@gmail.com>
2022-06-07 15:53:41 +0300

    Update main.c

--- ADC_multi_channels/main.c
+++ ADC_multi_channels/main.c
@@ -13,15 +13,14 @@ int main(void) {
 	 -------- Inits --------- 
 	UART_Init(MYUBRR);
 	ADC_Init();
-  printf(Hey Impn);
+  printf("Hey Imp\n");
 
 	while (1) {
     int	pot1 = readADC(PC0);
     int	pot2 = readADC(PC1);
-	int	pot3 = readADC(PC2);
-	printf(Pot 1 is %d  , pot1);
-	printf(Pot 2 is %d  , pot2);
-	printf(Pot 3 is %d  n, pot3);
-	 
-		} 		
-	}
+    int	pot3 = readADC(PC2);
+    printf(Pot 1 is %d  , pot1);
+    printf(Pot 2 is %d  , pot2);
+    printf(Pot 3 is %d  n, pot3);	 
+    } 		
+  }
