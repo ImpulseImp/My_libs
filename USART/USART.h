bf0f7f8179d1f3d87185bf141c4169eddc8d8d71
ImpulseImp <ImpulsEmbedded@gmail.com>
2022-06-07 16:14:21 +0300

    Update USART.h

--- USART/USART.h
+++ USART/USART.h
@@ -6,8 +6,8 @@
  */ 
 
 
-#ifndef UART_H_
-#define UART_H_
+#ifndef USART_H_
+#define USART_H_
 
 #define BAUD 9600
 #define MYUBRR F_CPU/16/BAUD-1
@@ -21,4 +21,4 @@ void USART_Init(unsigned int ubrr_val);
 int USART_putchar(char c, FILE *stream);
 
 
-#endif /* UART_H_ */
+#endif /* USART_H_ */
